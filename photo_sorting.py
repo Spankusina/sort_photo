c40ba7a79b43e248c83195ddecbbcc063f0d0822
Spankusina <chaplinskiysv@gmail.com>
2022-08-25 16:14:58 +0700

    upd

--- photo_sorting.py
+++ photo_sorting.py
@@ -1,5 +1,10 @@
 import os
+import datetime
 from exif import Image
+
+def md(path):
+    if os.path.exists(path) == False:
+        os.makedirs(path)
 
 path_in_dir = ''
 
@@ -9,20 +14,38 @@ while os.path.exists(path_in_dir) == False:
         print('Адрес не верный')
 
 path_out_dir = input('Введите адрес папки, куда сложить фотографии: ')
-if os.path.exists(path_out_dir) == False:
-    os.makedirs(path_out_dir)
+md(path_out_dir)
+
 count = 0
 
 for adress, dirs, files in os.walk(path_in_dir):
     for photo in files:
         if '.jpg' in photo or '.jpeg' in photo:
             full_path_photo = os.path.join(adress, photo)
+            print(full_path_photo)
+            
             with open(full_path_photo, "rb") as palm_file:
                 images = Image(palm_file)
-            data_photo = images.datetime_original
-            year_photo = data_photo[0:4]
-            month_photo = data_photo[5:7]
-            print(year_photo, month_photo)
+
+            if images.has_exif:
+                data_photo = images.datetime_original
+                year_photo = data_photo[0:4]
+                month_photo = data_photo[5:7]
+            else:
+                data_photo = datetime.datetime.fromtimestamp(os.path.getmtime(full_path_photo))
+                year_photo = str(data_photo.year)
+                if data_photo.month < 10:
+                    month_photo = '0' + str(data_photo.month)
+                else:
+                    month_photo = str(data_photo.month)
+            
+            palm_file.close
+
+            new_path_dir = os.path.join(path_out_dir, year_photo, month_photo)
+            print(new_path_dir)
+
+            md(new_path_dir)
+
             
 
 
