fc22bda27774f2a9753ffb3cc4aa592a4884a44c
Spankusina <chaplinskiysv@gmail.com>
2022-08-26 21:43:21 +0700

    upd

--- photo_sorting.py
+++ photo_sorting.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import hashlib 
+import shutil
 from exif import Image
 
 def md(path):
@@ -18,22 +19,35 @@ while os.path.exists(path_in_dir) == False:
 path_out_dir = input('Введите адрес папки, куда сложить фотографии: ')
 md(path_out_dir)
 
-count = 0
+#заносим хеш всех фотографий в конечной папке
+for adress, dirs, files in os.walk(path_out_dir):
+    for photo in files:
+        if '.jpg' in photo or '.jpeg' in photo or '.JPG' in photo or '.JPEG' in photo:
+            full_path_photo = os.path.join(adress, photo)
+            with open(full_path_photo, 'rb') as palm_file:
+                data = palm_file.read()
+            
+            file_hash = hashlib.md5()
+            file_hash.update(data)
+            if file_hash.hexdigest() not in hash_list:
+                hash_list.append(file_hash.hexdigest())
+
 
 for adress, dirs, files in os.walk(path_in_dir):
     for photo in files:
-        if '.jpg' in photo or '.jpeg' in photo:
+        if '.jpg' in photo or '.jpeg' in photo or '.JPG' in photo or '.JPEG' in photo:
             full_path_photo = os.path.join(adress, photo)
-            print(full_path_photo)
             
             with open(full_path_photo, 'rb') as palm_file:
                 data = palm_file.read()
-                images = Image(palm_file)
-                
-            file_hash = hashlib.md5()
-            file_hash.update(data)
-            hash_list.append(file_hash.hexdigest())
 
+                file_hash = hashlib.md5()
+                file_hash.update(data)
+
+                palm_file.seek(0,0)
+                images = Image(palm_file)
+
+          
             if images.has_exif:
                 data_photo = images.datetime_original
                 year_photo = data_photo[0:4]
@@ -45,15 +59,34 @@ for adress, dirs, files in os.walk(path_in_dir):
                     month_photo = '0' + str(data_photo.month)
                 else:
                     month_photo = str(data_photo.month)
-            
 
-            palm_file.close
+            new_adress = os.path.join(path_out_dir, year_photo, month_photo)
+            md(new_adress)
 
-            new_path_dir = os.path.join(path_out_dir, year_photo, month_photo)
-            print(new_path_dir)
+            #Если есть хеш фотографии в списке, тогда меняем название
+            if file_hash.hexdigest() in hash_list:
+                if '.jpg' in photo:
+                    extension = '.jpg'
+                elif '.JPG' in photo:
+                    extension = '.JPG'
+                elif '.jpeg' in photo:
+                    extension = '.jpeg'
+                elif '.JPEG' in photo:
+                    extension = '.JPEG'
+                
+                replica = 1    
+                photo = photo.replace(extension, ' (copy ' + str(replica) + ')' + extension)
+                check_new_name_photo = os.path.join(new_adress, photo)
+                while os.path.exists(check_new_name_photo):
+                    replica += 1
+                    photo = photo.replace('copy ' + str(replica-1), 'copy ' + str(replica))
+                    check_new_name_photo = os.path.join(new_adress, photo)
 
-            md(new_path_dir)
+                new_name_photo = os.path.join(adress, photo)
+                os.rename(full_path_photo, new_name_photo)
+                full_path_photo = new_name_photo
+            else:
+                hash_list.append(file_hash.hexdigest())
 
-        
-print(hash_list)
+            shutil.move(full_path_photo, new_adress)
 
